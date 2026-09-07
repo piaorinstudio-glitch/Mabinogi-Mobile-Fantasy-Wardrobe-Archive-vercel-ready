91895d28973d83d41ce97cfb40221f100ece4ec0
piaorinstudio-glitch <piaorinstudio@gmail.com>
2026-09-07 22:48:56 +0800

    Update ads-config.js

--- data/ads-config.js
+++ data/ads-config.js
@@ -1,7 +1,7 @@
 // Google AdSense switchboard.
 // Keep enabled:false while testing. After AdSense approval, set enabled:true.
 window.MABI_ADS_CONFIG={
-  enabled:false,
+  enabled:true,
   client:'ca-pub-5116561987707056',
   autoAds:true
 };
